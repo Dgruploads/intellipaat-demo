99527715da9092844e7fc29510bf4d193a02f530
Rabi <Rabi0123br@gmail.com>
2023-04-23 16:40:22 +0000

    Revert "added variable block to terraform file"
    This reverts commit d0982efe8470c6d561901f71021ae1a3d5946b84.

--- terraform.tf
+++ terraform.tf
@@ -3,18 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example_instance" {
-  instance_type = var.ec2_instance_type
-  ami           = var.ec2_image
-}
-
-variable "ec2_instance_type" {
-  type = string
-  description = "Instance type for the server"
-  default     = "t2.micro"
-}
-
-variable "ec2_image" {
-  type = string
-  description = "AMI for the server"
-  default     = "ami-007855ac798b5175e"
+  instance_type = "t2.micro"
+  ami           = "ami-007855ac798b5175e"
 }
