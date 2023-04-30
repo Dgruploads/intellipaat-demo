886f0ee9ab79687e1e0281cac5d86c2a9ef64500
Rabi <Rabi0123br@gmail.com>
2023-04-30 14:49:58 +0000

    variable block in feature branch

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "test_variable" {
+  type = string
+  description = "This is a test variable"
+}
+
 resource "aws_instance" "example_instance" {
   instance_type = "t2.micro"
   ami           = "ami-007855ac798b5175e"
