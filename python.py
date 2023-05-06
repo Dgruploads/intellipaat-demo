7b724871d0f542ab29346efe1c493accbeaefe8f
Rabi <Rabi0123br@gmail.com>
2023-05-06 14:50:43 +0000

    Dev 1 changes

--- python.py
+++ python.py
@@ -10,3 +10,14 @@ strings = ["one","two","three"]
 
 for str in strings:
 	print (str)
+
+
+#### Dev1 changes ######
+str = "This is an example code and the code is written in python"
+
+count = 0
+str_split = str.split(" ")
+for chars in str_split:
+  if chars == "code":
+    count += 1
+print (count)
